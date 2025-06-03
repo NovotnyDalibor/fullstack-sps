aef49474929d56aec0d4f1f5c881cc100224f1d2
NovotnyDalibor <dalibor.novotny@sudent.sps-cl.cz>
2025-06-03 19:49:13 +0200

    tabulky sql

--- database/seed.sql
+++ database/seed.sql
@@ -1,5 +1,34 @@
-INSERT INTO items (name, description) VALUES 
-('Nakoupit mléko', 'Mléko pro dort'),
-('Vyprat rty', 'Rty jsou plné'),
-('Nakoupit chleb', 'Chleb pro dort'),
-('Nakoupit jídlo pro psa', 'Pepa potřebuje jídlo');
+INSERT INTO places (name, row, column) VALUES ('A1', 1, 1);
+INSERT INTO places (name, row, column) VALUES ('A2', 1, 2);
+INSERT INTO places (name, row, column) VALUES ('A3', 1, 3);
+INSERT INTO places (name, row, column) VALUES ('A4', 1, 4);
+INSERT INTO places (name, row, column) VALUES ('A5', 1, 5);
+INSERT INTO places (name, row, column) VALUES ('A6', 1, 6);
+
+INSERT INTO places (name, row, column) VALUES ('B1', 2, 1);
+INSERT INTO places (name, row, column) VALUES ('B2', 2, 2);
+INSERT INTO places (name, row, column) VALUES ('B3', 2, 3);
+INSERT INTO places (name, row, column) VALUES ('B4', 2, 4);
+INSERT INTO places (name, row, column) VALUES ('B5', 2, 5);
+INSERT INTO places (name, row, column) VALUES ('B6', 2, 6);
+
+INSERT INTO places (name, row, column) VALUES ('C1', 3, 1);
+INSERT INTO places (name, row, column) VALUES ('C2', 3, 2);
+INSERT INTO places (name, row, column) VALUES ('C3', 3, 3);
+INSERT INTO places (name, row, column) VALUES ('C4', 3, 4);
+INSERT INTO places (name, row, column) VALUES ('C5', 3, 5);
+INSERT INTO places (name, row, column) VALUES ('C6', 3, 6);
+
+INSERT INTO places (name, row, column) VALUES ('D1', 4, 1);
+INSERT INTO places (name, row, column) VALUES ('D2', 4, 2);
+INSERT INTO places (name, row, column) VALUES ('D3', 4, 3);
+INSERT INTO places (name, row, column) VALUES ('D4', 4, 4);
+INSERT INTO places (name, row, column) VALUES ('D5', 4, 5);
+INSERT INTO places (name, row, column) VALUES ('D6', 4, 6);
+
+INSERT INTO places (name, row, column) VALUES ('E1', 5, 1);
+INSERT INTO places (name, row, column) VALUES ('E2', 5, 2);
+INSERT INTO places (name, row, column) VALUES ('E3', 5, 3);
+INSERT INTO places (name, row, column) VALUES ('E4', 5, 4);
+INSERT INTO places (name, row, column) VALUES ('E5', 5, 5);
+INSERT INTO places (name, row, column) VALUES ('E6', 5, 6);
